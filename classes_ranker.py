8c43d691704fe559f6e0640393f940f2cfd48ba9
mfbuit46 <mfbuit46@mit.edu>
2024-11-20 18:11:20 -0500

    added incompleteness check

--- classes_ranker.py
+++ classes_ranker.py
@@ -1,36 +1,18 @@
 import z3
-from random import shuffle
 import os
 current_dir = os.path.dirname(__file__)
 
-def make_pairs(classes):
-    pairs = []
-    for class1 in classes:
-        for class2 in classes:
-            if (class2, class1) not in pairs and class1 != class2:
-                pairs.append((class1, class2))
-    shuffle(pairs)
-    return pairs
-
-def make_classes_csv(classes, file_name, run=True):
-    pairs = make_pairs(classes)
-    csv_text = ""
-    for pair in pairs:
-        csv_text += f"{pair[0]},{pair[1]},\n"
-    if not run: # run param ensures that preference csv is not overwritten if set to False
-        print(f"NOTE: 'run' parameter for make_classes_cv is set to False, so no csv was made")
-        return
-    # in case you don't set run=False but don't want to overwrite, we have this try block
-    try: 
-        new_file = open(file_name,'x')
-        new_file.write(csv_text)
-        new_file.close()
-    except:
-        overwrite = input(f"file {file_name} already exists. Do you wish to overwrite it? Type Y for yes or any other key for no:")
-        if overwrite == 'Y':
-            new_file = open(file_name,'w')
-            new_file.write(csv_text)
-            new_file.close()
+# z3 basic definitions
+ClassSort = z3.DeclareSort("ClassSort")
+is_better_than = z3.Function("is_better_than", ClassSort, ClassSort, z3.BitVecSort(1))
+x,y,z = z3.Consts('x y z', ClassSort)
+transitivity = z3.ForAll([x,y,z], z3.Implies(z3.And(is_better_than(x,y)==1, is_better_than(y,z)==1), is_better_than(x,z)==1))
+asymmetry = z3.ForAll([x,y], z3.Implies(is_better_than(x,y)==1, is_better_than(y,x)==0)) # asym (derives reflexivity)
+completeness = z3.ForAll([x,y,z], z3.Implies(is_better_than(y,z)==1,
+                                                z3.Or(is_better_than(x,y)==1,
+                                                    is_better_than(y,x)==1,
+                                                    is_better_than(x,z)==1,
+                                                    is_better_than(z,x)==1,)))
 
 def get_dsv_text_and_type(file_name):
     """
@@ -45,14 +27,16 @@ def get_dsv_text_and_type(file_name):
     file_type = file_name[-3:]
     return file_text, file_type
 
-def process_data(file_name):
+def process_data(file_name, only_positive_judgements=False):
     data = get_dsv_text_and_type(file_name)[0]
     data_as_list = data.split('\n')
     processed_data = []
     for row in data_as_list:
         row_as_list = row.split(',')
         assert len(row_as_list) == 3, row_as_list
-        if row_as_list[2] != '':
+        if not only_positive_judgements:
+            processed_data.append(row_as_list)
+        elif row_as_list[2] != '':
             processed_data.append(row_as_list)
     return processed_data
 
@@ -67,64 +51,30 @@ def make_relations_set(processed_data):
         relations_set.add((better_op, worse_op))
     return relations_set
 
-# def process_data(file_name):
-#     data = get_dsv_text_and_type(file_name)[0]
-#     data_as_list = data.split('\n')
-#     processed_data = []
-#     for row in data_as_list:
-#         row_as_list = row.split(',')
-#         assert len(row_as_list) == 3, row_as_list
-#         processed_data.append(row_as_list)
-#     return processed_data
-
-# errmsg = lambda x, y, z: f"the provided preference {z} is not one of the options in comparison ({x}, {y})"
-
-# def make_relations_set(processed_data):
-#     relations_set = set()
-#     for relation in processed_data: # comparison is a list of 3 strings (representing classes)
-#         op1, op2, comparison = relation
-
-#         assert comparison in (op1, op2) or comparison == '-' or comparison == '', errmsg(op1, op2, better_op)
-#         if comparison in (op1, op2):
-#             worse_op = op1 if comparison == op2 else op2
-#             relations_set.add((comparison, worse_op))
-#     return relations_set
-
 def solve(solver, constraints):
     result = solver.check(constraints)
     if result == z3.sat:
         return True, solver.model()
     return False, solver.unsat_core()
 
-def find_better(relations_set):
-    ClassSort = z3.DeclareSort("ClassSort")
-    is_better_than = z3.Function("is_better_than", ClassSort, ClassSort, z3.BitVecSort(1))
+def solve_consistency(file_name, add_incompleteness=False):
+    processed_data = process_data(file_name, only_positive_judgements=True)
+    if len(processed_data) == 0:
+        return f"no data in the file {file_name} so far"
+    relations_set = make_relations_set(processed_data)
     constraints = []
-    # for relation in relations_set:
-    #     class1_str, class2_str = relation
-    #     cs1, cs2 = z3.Const(class1_str, ClassSort), z3.Const(class2_str, ClassSort)
-    #     constraints.append(is_better_than(cs1, cs2)==1)
     for relation in relations_set:
         class1_str, class2_str = relation
         cs1, cs2 = z3.Const(class1_str, ClassSort), z3.Const(class2_str, ClassSort)
         constraints.append(is_better_than(cs1, cs2)==1)
-        # if comparison_str in (class1_str, class2_str):
-        #     worse_op = class1_str if comparison_str == class2_str else class2_str
-        #     cs1, cs2 = z3.Const(comparison_str, ClassSort), z3.Const(worse_op, ClassSort)
-        #     constraints.append(is_better_than())
-    x,y,z = z3.Consts('x y z', ClassSort)
-    transitivity = z3.ForAll([x,y,z], z3.Implies(z3.And(is_better_than(x,y)==1, is_better_than(y,z)==1), is_better_than(x,z)==1))
-    asymmetry = z3.ForAll([x,y], z3.Implies(is_better_than(x,y)==1, is_better_than(y,x)==0)) # asym (derives reflexivity)
-    completeness = z3.ForAll([x,y,z], z3.Implies(is_better_than(y,z)==1,
-                                                 z3.Or(is_better_than(x,y)==1,
-                                                       is_better_than(y,x)==1,
-                                                       is_better_than(x,z)==1,
-                                                       is_better_than(z,x)==1,)))
     constraints.append(transitivity)
     constraints.append(asymmetry)
-    # constraints.append(z3.Not(completeness))
+    if add_incompleteness:
+        constraints.append(z3.Not(completeness))
     bsolver = z3.Solver()
     return solve(bsolver, constraints)
+    
+# def solve_incompleteness(processed_data)
 
 def assign_values(bmodel):
     ClassSort = bmodel.get_sort(0)
@@ -146,11 +96,7 @@ def assign_values(bmodel):
     return solve(vsolver, constraints)
 
 def find_topological_order(file_name):
-    processed_data = process_data(file_name)
-    if len(processed_data) == 0:
-        return f"no data in the file {file_name} so far"
-    relations_set = make_relations_set(processed_data)
-    bsat, bmodel = find_better(relations_set)
+    bsat, bmodel = solve_consistency(file_name)
     if bsat:
         vsat, vmodel = assign_values(bmodel)
     else:
@@ -174,29 +120,49 @@ def find_topological_order(file_name):
     else:
         unsat_core = vmodel
         return unsat_core
-    
 
-
-# put your classes in this list
-classes = ["18.02", "8.01", "24.900", "5.111", "21G.401",
-           "18.06", "8.02", "24.902", "21G.402", "6.100L", 
-           "6.370", "6.101", "24.951", "24.141", "3.983", 
-           "6.390", "24.912", "24.280", "24.952", "6.120", 
-           "6.861", "24.970", "24.260", "24.211"]
-# put whatever you want the file name and directory to be
 file_name = current_dir+'/classes_pairs.csv'
 
-# STEP 1: make the csv file with all class pairs. 
-# edit this file so that after each pair (e.g. '18.02,8.01'), put your preference between the two
-# (e.g. '18.02,8.01,18.02'). If you have no clear preference, leave it blank (you can also put
-# '-', as in '18.02,8.01,-'). This will not ensure the two have the same ranking; it just will
-# treat the comparison as if there was not a dash (i.e. it will ignore it)
-make_classes_csv(classes, file_name, run=False)
+# # STEP 2: run this line (you can run it even if you haven't filled any of the csv out)
+# result = find_topological_order(current_dir+'/classes_pairs.csv')
+# if not isinstance(result, dict):
+#     print(result)
+# else:
+#     print("preferences are self-consistent!")
+# print(result)
 
-# STEP 2: run this line (you can run it even if you haven't filled any of the csv out)
-result = find_topological_order(file_name)
-if not isinstance(result, dict):
-    print(result)
-else:
-    print("preferences are self-consistent!")
-print(result)
+def find_incomplete_preferences(file_name):
+    bsat, bmodel = solve_consistency(file_name, add_incompleteness=True)
+    if not bsat:
+        return bmodel
+    # can assume we now have a model
+    Classes_list = []
+    for i in range(len(bmodel)):
+        if isinstance(bmodel[bmodel[i]], z3.ExprRef):
+            source_str_name = str(bmodel[i])
+            Classes_list.append(z3.Const(source_str_name, ClassSort))
+    incomplete_prefs = []
+    for class1 in Classes_list:
+        for class2 in Classes_list:
+            for class3 in Classes_list:
+                if str(class1) != str(class2) and str(class2) != str(class3) and str(class1) != str(class3):
+                    incompleteness_inst = z3.And(is_better_than(class2,class3)==1,
+                                                    is_better_than(class1,class2)==0,
+                                                    is_better_than(class2,class1)==0,
+                                                    is_better_than(class1,class3)==0,
+                                                    is_better_than(class3,class1)==0,)
+                    if bmodel.evaluate(incompleteness_inst):
+                        incomplete_prefs.append((class1, class2, class3))
+    return incomplete_prefs            
+
+def print_incomplete_preferences(file_name):
+    incomplete_preferences = find_incomplete_preferences(file_name)
+    if not incomplete_preferences:
+        print("there are no incomplete preferences")
+    # can assume there are incomplete preferences
+    for c1, c2, c3 in incomplete_preferences:
+        print(f"\t{c2}\n{c1}\tv\n\t{c3}\n\n")
+
+
+print_incomplete_preferences(file_name)
+    
---
+++ make_classes_csv.py
@@ -0,0 +1,48 @@
+from random import shuffle
+import os
+current_dir = os.path.dirname(__file__)
+
+def make_pairs(classes):
+    pairs = []
+    for class1 in classes:
+        for class2 in classes:
+            if (class2, class1) not in pairs and class1 != class2:
+                pairs.append((class1, class2))
+    shuffle(pairs)
+    return pairs
+
+def make_classes_csv(classes, file_name, run=True):
+    pairs = make_pairs(classes)
+    csv_text = ""
+    for pair in pairs:
+        csv_text += f"{pair[0]},{pair[1]},\n"
+    if not run: # run param ensures that preference csv is not overwritten if set to False
+        print(f"NOTE: 'run' parameter for make_classes_cv is set to False, so no csv was made")
+        return
+    # in case you don't set run=False but don't want to overwrite, we have this try block
+    try: 
+        new_file = open(file_name,'x')
+        new_file.write(csv_text)
+        new_file.close()
+    except:
+        overwrite = input(f"file {file_name} already exists. Do you wish to overwrite it? Type Y for yes or any other key for no:")
+        if overwrite == 'Y':
+            new_file = open(file_name,'w')
+            new_file.write(csv_text)
+            new_file.close()
+
+# put your classes in this list
+classes = ["18.02", "8.01", "24.900", "5.111", "21G.401",
+           "18.06", "8.02", "24.902", "21G.402", "6.100L", 
+           "6.370", "6.101", "24.951", "24.141", "3.983", 
+           "6.390", "24.912", "24.280", "24.952", "6.120", 
+           "6.861", "24.970", "24.260", "24.211"]
+# put whatever you want the file name and directory to be
+file_name = current_dir+'/classes_pairs.csv'
+
+# STEP 1: make the csv file with all class pairs. 
+# edit this file so that after each pair (e.g. '18.02,8.01'), put your preference between the two
+# (e.g. '18.02,8.01,18.02'). If you have no clear preference, leave it blank (you can also put
+# '-', as in '18.02,8.01,-'). This will not ensure the two have the same ranking; it just will
+# treat the comparison as if there was not a dash (i.e. it will ignore it)
+make_classes_csv(classes, file_name, run=False)
